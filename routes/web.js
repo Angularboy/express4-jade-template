29c15108f67157aa2b76ad7d778e7946c24347f1
liuyiqiao <liuyiqiao@ftxmall.com>
2017-09-18 16:13:28 +0800

    修改

--- routes/web.js
+++ routes/web.js
@@ -4,7 +4,13 @@ var router = express.Router();
 var routeDoc = {doc_content: CONFIG.doc_content};
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    routeDoc.route = 'index';
     res.render('index', routeDoc);
+});
+/* GET about page. */
+router.get('/about', function (req, res, next) {
+    routeDoc.route = 'about';
+    res.render('about', routeDoc);
 });
 
 module.exports = router;
